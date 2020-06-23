ce82793029a4e25ba4316b725014e3edaee85c4f
tebmik <lloyd.s.tom@gmail.com>
2020-06-23 10:58:28 +0100

    added cleanUp action to remove verification error/success mesaage when component is unmounted

--- src/containers/auth/verifyEmail/VerifyEmail.js
+++ src/containers/auth/verifyEmail/VerifyEmail.js
@@ -1,58 +1,61 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styled from "styled-components";
-import {
-  PageWrapper,
-  FormWrapper,
-  FormHeader,
-  MessageWrapper,
-} from "../../../hoc/containers";
+import {PageWrapper, FormWrapper, FormHeader, MessageWrapper} from "../../../hoc/containers";
 import Button from "../../../components/buttons/Button";
 import Message from "../../../components/ui/message/Message";
 
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import * as actions from "../../../actions";
 
-const StyledContent = styled.div`
+const StyledContent = styled.div `
   min-height: 30rem;
 `;
 
-const VerifyEmail = ({ sendVerification, error, loading }) => {
-  console.log(error);
-  console.log(loading);
-  return (
-    <PageWrapper>
-      <FormWrapper>
-        <FormHeader>Time to verify your email.</FormHeader>
-        <StyledContent>
-          Verify you email and gain access to all functionality of this
-          application.
-          <Button
-            disabled={loading}
-            onClick={() => sendVerification()}
-            type="submit"
-            title={loading ? "Sending Email..." : "Re-send verification email"}
-          />
-        </StyledContent>
-        <MessageWrapper>
-          <Message error show={error}>
-            {error}
-          </Message>
-          <Message success show={error === false}>
-            Email sent successfully.
-          </Message>
-        </MessageWrapper>
-      </FormWrapper>
-    </PageWrapper>
-  );
+const VerifyEmail = ({sendVerification, error, loading, cleanUp}) => {
+
+    useEffect(() => {
+        return() => {
+            cleanUp()
+        };
+    }, [cleanUp]);
+
+    return (
+        <PageWrapper>
+            <FormWrapper>
+                <FormHeader>Time to verify your email.</FormHeader>
+                <StyledContent>
+                    Verify you email and gain access to all functionality of this
+                                                                      application.
+                    <Button disabled={loading}
+                        onClick={
+                            () => sendVerification()
+                        }
+                        type="submit"
+                        title={
+                            loading ? "Sending Email..." : "Re-send verification email"
+                        }/>
+                </StyledContent>
+                <MessageWrapper>
+                    <Message error
+                        show={error}>
+                        {error} </Message>
+                    <Message success
+                        show={
+                            error === false
+                    }>
+                        Email sent successfully.
+                    </Message>
+                </MessageWrapper>
+            </FormWrapper>
+        </PageWrapper>
+    );
 };
 
-const mapStateToProps = ({ auth }) => ({
-  error: auth.verifyEmail.error,
-  loading: auth.verifyEmail.loading,
-});
+const mapStateToProps = ({auth}) => ({error: auth.verifyEmail.error, loading: auth.verifyEmail.loading});
 
 const mapDispatchToProps = {
-  sendVerification: actions.verifyEmail,
+    sendVerification: actions.verifyEmail,
+    cleanUp: actions.cleanUp
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(VerifyEmail);
