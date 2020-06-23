b7dba9809f7af6e20d00a70ed8c161d9cba2006f
tebmik <lloyd.s.tom@gmail.com>
2020-06-23 11:03:10 +0100

    Set the global FormWrapper styled-component to 'relative' and removed zombie code

--- src/hoc/containers/index.js
+++ src/hoc/containers/index.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import { Form } from "formik";
+import {Form} from "formik";
 
-export const PageWrapper = styled.div`
+export const PageWrapper = styled.div `
   min-height: 100vh;
   width: 100%;
   display: flex;
@@ -9,7 +9,7 @@ export const PageWrapper = styled.div`
   align-items: center;
 `;
 
-export const SignupWrapper = styled.div`
+export const SignupWrapper = styled.div `
   position: relative;
   display: flex;
   justify-content: center;
@@ -20,14 +20,12 @@ export const SignupWrapper = styled.div`
   margin: 0 auto;
 `;
 
-export const FormWrapper = styled.div`
+export const FormWrapper = styled.div `
   min-width: 32rem;
   max-width: 90rem;
   width: 100%;
   margin: 0 auto;
   border-radius: 1rem;
-  // padding: 3rem 2rem;
-  // display: flex;
   flex-direction: column;
   align-items: center;
   justify-items: center;
@@ -36,10 +34,13 @@ export const FormWrapper = styled.div`
   overflow: hidden;
   position: relative;
   min-height: 60rem;
-  display: ${(props) => (props.formClose ? "none" : "flex")};
+  display: ${
+    (props) => (props.formClose ? "none" : "flex")
+};
+  position: relative;
 `;
 
-export const FormHeader = styled.div`
+export const FormHeader = styled.div `
   height: 100%;
   background-color: var(--color-info);
   width: 100%;
@@ -57,7 +58,7 @@ export const StyledForm = styled(Form)`
   position: relative;
 `;
 
-export const MessageWrapper = styled.div`
+export const MessageWrapper = styled.div `
   position: absolute;
   bottom: 5.5rem;
   text-align: center;
