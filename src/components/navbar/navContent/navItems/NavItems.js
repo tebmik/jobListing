4241a999441a84d69699d5a45bd25f33a3a07258
tebmik <lloyd.s.tom@gmail.com>
2020-06-23 10:55:58 +0100

    removed Contact link from navItems

--- src/components/navbar/navContent/navItems/NavItems.js
+++ src/components/navbar/navContent/navItems/NavItems.js
@@ -3,75 +3,78 @@ import styled from "styled-components";
 
 import NavItem from "./navItem/NavItem";
 
-const Wrapper = styled.div`
+const Wrapper = styled.div `
   display: flex;
 `;
 
-const Ul = styled.ul`
+const Ul = styled.ul `
   display: flex;
-  flex-direction: ${(props) => (props.mobile ? "column" : "row")};
-  align-items: ${(props) => (props.mobile ? "start" : "center")};
-  justify-content: ${(props) => (props.mobile ? "space-around" : "end")};
+  flex-direction: ${
+    (props) => (props.mobile ? "column" : "row")
+};
+  align-items: ${
+    (props) => (props.mobile ? "start" : "center")
+};
+  justify-content: ${
+    (props) => (props.mobile ? "space-around" : "end")
+};
   height: 100%;
 `;
 
-const NavItems = ({ clicked, mobile, loggedIn, emailVerified }) => {
-  let links;
-  if (loggedIn && !emailVerified) {
-    links = (
-      <>
-        {/* <NavItem mobile={mobile} clicked={clicked} title="Home" link="/" /> */}
-        <NavItem
-          mobile={mobile}
-          clicked={clicked}
-          title="Logout"
-          link="/logout"
-        />
-      </>
-    );
-  } else if (loggedIn && emailVerified) {
-    links = (
-      <>
-        <NavItem mobile={mobile} clicked={clicked} title="Home" link="/" />
-        <NavItem
-          mobile={mobile}
-          clicked={clicked}
-          title="Profile"
-          link="/profile"
-        />
-        <NavItem
+const NavItems = ({clicked, mobile, loggedIn, emailVerified}) => {
+    let links;
+    if (loggedIn && !emailVerified) {
+        links = (
+            <> {/* <NavItem mobile={mobile} clicked={clicked} title="Home" link="/" /> */}
+                <NavItem mobile={mobile}
+                    clicked={clicked}
+                    title="Logout"
+                    link="/logout"/>
+            </>
+        );
+    } else if (loggedIn && emailVerified) {
+        links = (
+            <>
+                <NavItem mobile={mobile}
+                    clicked={clicked}
+                    title="Home"
+                    link="/"/>
+                <NavItem mobile={mobile}
+                    clicked={clicked}
+                    title="Profile"
+                    link="/profile"/> {/* <NavItem
           mobile={mobile}
           clicked={clicked}
           title="Contact"
           link="/contact"
-        />
-        <NavItem
-          mobile={mobile}
-          clicked={clicked}
-          title="Logout"
-          link="/logout"
-        />
-      </>
-    );
-  } else {
-    links = (
-      <>
-        <NavItem mobile={mobile} clicked={clicked} title="Home" link="/" />
-        <NavItem
-          mobile={mobile}
-          clicked={clicked}
-          title="Signup"
-          link="/signup"
-        />
-      </>
-    );
-  }
+        /> */}
+                <NavItem mobile={mobile}
+                    clicked={clicked}
+                    title="Logout"
+                    link="/logout"/>
+            </>
+        );
+    } else {
+        links = (
+            <>
+                <NavItem mobile={mobile}
+                    clicked={clicked}
+                    title="Home"
+                    link="/"/>
+                <NavItem mobile={mobile}
+                    clicked={clicked}
+                    title="Signup"
+                    link="/signup"/>
+            </>
+        );
+    }
 
-  return (
-    <Wrapper mobile={mobile}>
-      <Ul mobile={mobile}>{links}</Ul>
-    </Wrapper>
-  );
+    return (
+        <Wrapper mobile={mobile}>
+            <Ul mobile={mobile}>
+                {links}</Ul>
+        </Wrapper>
+    );
 };
 
 export default NavItems;
