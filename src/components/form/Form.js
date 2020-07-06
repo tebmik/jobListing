79faafa64b1d5715bb062eca7f10f8801ec01457
tebmik <lloyd.s.tom@gmail.com>
2020-07-06 12:26:54 +0100

    useRef's defined to operate onKeyDown, css and animation classes

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -3,66 +3,108 @@ import "animate.css";
 import styled from "styled-components";
 
 import Input from "../inputs/Input";
-import Label from "../labels/Label";
+// import Label from "../labels/Label";
 import Button from "../buttons/Button";
-import { FormHeader } from "../../hoc/containers";
+import { PageWrapper, StyledUserName } from "../../hoc/containers";
 
 import { connect } from "react-redux";
 import * as actions from "../../actions";
 
 const FormWrapper = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   font-size: 1.6rem;
   min-height: 50rem;
   min-width: 30rem;
-  max-width: 50rem;
+  max-width: 120rem;
   width: 100%;
-  border-radius: 2rem;
-  overflow: hidden;
-  display: ${(props) => (props.formClose ? "none" : "block")};
+  border-radius: 1rem;
+  display: ${(props) => (props.formClose ? "none" : "grid")};
   background-color: var(--color-white);
-  box-shadow: 0rem 0.5rem 3.5rem var(--shadow);
+  // box-shadow: 0rem 0.5rem 3.5rem var(--shadow);
+  grid-template-columns: 1fr 1fr;
+  justify-items: center;
+
+
+  @media only screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 const StyledForm = styled.form`
   display: grid;
   grid-template-columns: 1fr;
-  grid-gap: 1.5rem;
-  // justify-items: center;
+  justify-items: start;
+  width: calc(100% - 28%);
   align-self: center;
   padding: 2rem;
+  height: 100%;
+  box-shadow: 0rem 0.5rem 3.5rem var(--shadow);
+  background-color: var(--color-white);
+  border-radius: 1rem;
 `;
+
+const InputWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  width: 100%;
+  position: relative;
+  align-items: center;
+`;
+
+const WelcomeWrapper = styled.div`
+  padding: 2rem;
+  width: calc(100% - 28%);
+`;
+
+const P = styled.p`
+  font-size: 2.5vw;
+  font-weight: 300;
+  color: var(--color-text);
+  animation: backInUp;
+  animation-duration: 1s; /* don't forget to set a duration! */
+
+
+  @media only screen and (max-width: 768px) {
+    font-size: 2.2rem;
+    text-align: center;
+  }
+
+`;
+
 
 const Form = ({ error, loading, fetchData, successFetch, jobs, userName }) => {
   const [jobTerm, setJobTerm] = useState("");
   const [placeTerm, setPlaceTerm] = useState("");
   const [close, setClose] = useState(false);
 
-  //   const jobRef = useRef(null);
-  //   const placeRef = useRef(null);
-  //   const submitRef = useRef(null);
+  useEffect(() => {
+    whatRef.current.focus();
+  }, []);
+  const whatRef = useRef(null);
+  const whereRef = useRef(null);
+  const submitRef = useRef(null);
 
-  //   useEffect(() => {
-  //     jobRef.current.focus();
-  //   }, []);
-
-  // const handleJobKeydown = e => e.key === "Enter" ? placeRef.current.focus() : null;
-
-  // const handlePlaceKeydown = e => e.key === "Enter" ? submitRef.current.focus() : null;
-
-  // const handleSubmitKeydown = e => e.key === "Enter" ? console.log("form has been submitted") : null;
 
   const handleJobChange = (e) => {
     setJobTerm(e.target.value);
+  };
+  
+  const handleWhatKeyEnter = (e) => {
+    e.key === "Enter" && e.preventDefault();
+    if(e.key === "Enter") {
+      whereRef.current.focus();
+    }
   };
 
   const handlePlaceChange = (e) => {
     setPlaceTerm(e.target.value);
   };
 
+  const handleWhereKeyEnter = (e) => {
+    e.key === "Enter" && e.preventDefault();
+    if(e.key === "Enter") {
+      submitRef.current.focus();
+    }
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     setJobTerm(jobTerm);
@@ -74,74 +116,83 @@ const Form = ({ error, loading, fetchData, successFetch, jobs, userName }) => {
   };
 
   return (
-    <FormWrapper
+    <PageWrapper
       formClose={jobs.results ? () => setClose(!close) : false}
-      id="formWrapper"
-      className={jobs.results ? "animated animate__fadeOutDown" : null}
     >
-      {userName ? (
-        <FormHeader>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              // marginTop: "10rem",
-              position: "relative",
-            }}
-          >
-            <p>
-              Hi <p className="animate_animated animate__pulse">{userName}</p>,
-              I wish you the best of luck, finding your next career.
-            </p>
-          </div>
-        </FormHeader>
-      ) : (
-        <FormHeader>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              // marginTop: "10rem",
-              position: "relative",
-            }}
-          >
-            Hi there, I wish you the best of luck, finding your next career.
-          </div>
-        </FormHeader>
-      )}
 
-      <StyledForm id="form" onSubmit={handleSubmit}>
-        <Label title="What?" />
-        <Input
-          // onKeyDown={handleJobKeydown}
-          handleChange={handleJobChange}
-          value={jobTerm}
-          type="text"
-          placeholder="Type Career"
-          //   focusRef={jobRef}
-        />
-        <Label title="Where?" />
-        <Input
-          // onKeyDown={handlePlaceKeydown}
-          handleChange={handlePlaceChange}
-          value={placeTerm}
-          type="text"
-          placeholder="Type Region"
-          //   focusRef={placeRef}
-        />
-        <Button
-          // onKeyDown={handleSubmitKeydown}
-          disabled={loading}
-          loading={loading}
-          title={loading ? "Fetching..." : "Find Your Career"}
-          type="submit"
-          //   focusRef={submitRef}
-          handleSubmit={handleSubmit}
-        />
+      <FormWrapper
+        formClose={jobs.results ? () => setClose(!close) : false}
+        id="formWrapper"
+        className={jobs.results ? "animated animate__fadeOutDown" : null}
+      >
+        {userName ? (
+          <WelcomeWrapper>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  // marginTop: "10rem",
+                  position: "relative",
+                }}
+              >
+                <P>
+                  Hi <StyledUserName>{userName}</StyledUserName>,
+                  best of luck finding a new career.
+                </P>
+              </div>
+          </WelcomeWrapper>
 
-        {error ? <p>{error}</p> : null}
-      </StyledForm>
-    </FormWrapper>
+        ) : (
+          <WelcomeWrapper >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                // marginTop: "10rem",
+                position: "relative",
+              }}
+            >
+              <P className="animate__animated animate__fadeInUp">
+                Hi there, best of luck finding a new career.
+              </P>
+            </div>
+          </WelcomeWrapper>
+        )}
+
+        <StyledForm id="form" onSubmit={handleSubmit}>
+          <InputWrapper>
+            <Input
+              // onKeyDown={handleJobKeydown}
+              handleChange={handleJobChange}
+              value={jobTerm}
+              type="text"
+              placeholder="What"
+              focusRef={whatRef}
+              keyDown={handleWhatKeyEnter}
+            />
+            <Input
+              // onKeyDown={handlePlaceKeydown}
+              handleChange={handlePlaceChange}
+              value={placeTerm}
+              type="text"
+              placeholder="Where"
+              focusRef={whereRef}
+              keyDown={handleWhereKeyEnter}
+            />
+            <Button
+              keyDown={handleSubmit}
+              disabled={loading}
+              loading={loading}
+              title={loading ? "Fetching..." : "Find Your Career"}
+              type="submit"
+              focusRef={submitRef}
+            />  
+          </InputWrapper>
+
+          {error ? <p>{error}</p> : null}
+        </StyledForm>
+      </FormWrapper>
+    </PageWrapper>
   );
 };
 
